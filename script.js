65f3d0f99ae7b26467cb23ee76611c6f0ae67d22
adriano <eacbzs@gmail.com>
2023-02-14 21:52:23 -0300

    modal

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 function switchSheet() {
-    let theme = document.getElementById("tema");
-  
-    if (theme.getAttribute("href") == "estilo_claro.css") {
-      theme.href = "estilo_oscuro.css";
-    } else {
-      theme.href = "estilo_claro.css";
-    }
+  let theme = document.getElementById("tema");
+
+  if (theme.getAttribute("href") == "./styles/estilo_claro.css") {
+    theme.href = "./styles/estilo_oscuro.css";
+  } else {
+    theme.href = "./styles/estilo_claro.css";
   }
+}
+
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+  modal_container.classList.add('show');  
+});
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
