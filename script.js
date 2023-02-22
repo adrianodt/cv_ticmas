82764d3ff2b7ff436d2755db89f287b100ba384e
adriano <eacbzs@gmail.com>
2023-02-22 19:53:45 -0300

    herramientas

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ function switchSheet() {
 }
 
 const open = document.getElementById('open');
+const open2 = document.getElementById('open2');
 const modal_container = document.getElementById('modal_container');
+const modal_container2 = document.getElementById('modal_container2');
 const close = document.getElementById('close');
+const close2 = document.getElementById('close2');
 
 open.addEventListener('click', () => {
   modal_container.classList.add('show');  
@@ -18,4 +21,12 @@ open.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   modal_container.classList.remove('show');
+});
+
+open2.addEventListener('click', () => {
+  modal_container2.classList.add('show');  
+});
+
+close2.addEventListener('click', () => {
+  modal_container2.classList.remove('show');
 });
